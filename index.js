be759f08d1e62e2b29e5512d563af581216191d8
raj-j-shah <rajcoder19@gmail.com>
2023-10-08 19:27:04 +0530

    corrections

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.post('/calculate-emi', (req, res) => {
   const loanAmount = JSON.parse(req.body.queryResult.fulfillmentText).loanamount;
   const interestRate = 10 / 100; // Convert percentage to decimal
   const tenure = JSON.parse(req.body.queryResult.fulfillmentText).tenure;
-  console.log(req.body);
-  console.log(loanAmount+" ,"+tenure);
-console.log("here")
+//   console.log(req.body);
+//   console.log(loanAmount+" ,"+tenure);
+// console.log("here")
   const monthlyInterestRate = interestRate / 12;
   const numberOfPayments = tenure * 12;
 
@@ -18,7 +18,7 @@ console.log("here")
     (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
   const response = {
-    fulfillmentText: `Your EMI is $${emi.toFixed(2)} per month.`,
+    fulfillmentText: `Your EMI is Rs.${emi.toFixed(2)} per month.`,
   };
 
   res.json(response);
