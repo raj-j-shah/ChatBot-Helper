b09359d526985c102e8cc853e9cce28c9815aa7a
raj-j-shah <rajcoder19@gmail.com>
2023-10-09 11:16:25 +0530

    custom

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-
-app.post('/calculate-emi', (req, res) => {
+function calEmi(req){
   const loanAmount = JSON.parse(req.body.queryResult.fulfillmentText).loanamount;
   const interestRate = 10 / 100; // Convert percentage to decimal
   const tenure = JSON.parse(req.body.queryResult.fulfillmentText).tenure;
@@ -29,6 +28,19 @@ app.post('/calculate-emi', (req, res) => {
   const response = {
     fulfillmentText: `Your EMI is Rs.${finalEmi} per month.`,
   };
+  return response
+}
+app.post('/get-response', (req,res) => {
+  console.log(req.body);
+  if(req.body.queryResult.acion==="Two_wheeler.Two_wheeler-custom.Two_wheeler-custom-custom"){
+    res.json(calEmi(req));
+  }
+  if(req.body.queryResult.acion==="Four_wheeler.Four_wheeler-custom.Four_wheeler-custom-custom"){
+    res.json(calEmi(req));
+  }
+  if(req.body.queryResult.acion==="")
+
+
 
   res.json(response);
 });
