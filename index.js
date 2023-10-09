c71a5641c22f478503601d4e587747c14aea9d19
raj-j-shah <rajcoder19@gmail.com>
2023-10-09 11:43:02 +0530

    changes

--- index.js
+++ index.js
@@ -33,15 +33,15 @@ function calEmi(req){
 app.post('/get-response', (req,res) => {
 
   console.log(req.body);
-  if(req.body.queryResult.acion==="Two_wheeler.Two_wheeler-custom.Two_wheeler-custom-custom"){
+  if(req.body.queryResult.acion==='Two_wheeler.Two_wheeler-custom.Two_wheeler-custom-custom'){
     console.log("emi");
     res.json(calEmi(req));
   }
-  if(req.body.queryResult.acion==="Four_wheeler.Four_wheeler-custom.Four_wheeler-custom-custom"){
+  if(req.body.queryResult.acion==='Four_wheeler.Four_wheeler-custom.Four_wheeler-custom-custom'){
     console.log("emi");
     res.json(calEmi(req));
   }
-  if(req.body.queryResult.acion==="DefaultWelcomeIntent.DefaultWelcomeIntent-custom"){
+  if(req.body.queryResult.acion==='DefaultWelcomeIntent.DefaultWelcomeIntent-custom'){
     console.log("namei");
       console.log("name aaya");
   } 
