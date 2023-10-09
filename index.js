a5cbfcd2e27c8dc5913abad74a15b7f1294305c6
raj-j-shah <rajcoder19@gmail.com>
2023-10-09 20:35:29 +0530

    added name & phoneno save

--- index.js
+++ index.js
@@ -60,15 +60,16 @@ app.post('/get-response', async (req, res) => {
   else if ((req.body.queryResult).action === 'DefaultWelcomeIntent.DefaultWelcomeIntent-custom.Customer-info-custom-custom') {
     const pno = req.body.queryResult.parameters['phone-number'];
     var phoneNumberPattern = /^\d{10}$/;
-
+    console.log("pno");
     // Remove any non-digit characters from the input string
-    var cleanInput = input.replace(/\D/g, '');
+    var cleanInput = pno.replace(/\D/g, '');
 
     // Test if the cleaned input matches the pattern
     if (phoneNumberPattern.test(cleanInput)) {
       const ssid = req.body.session;
       const curr_user = await user.find({ "session": ssid });
       if (curr_user.length > 0) {
+        console.log("saving update....")
         const mod_userid = curr_user[0]._id;
         const mod_user_name = curr_user[0].user_name;
         user.findByIdAndUpdate(mod_userid, { "phone_no": cleanInput });
